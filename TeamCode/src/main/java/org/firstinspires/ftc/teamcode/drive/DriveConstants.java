90239d7268d70de75bf589f90930d9ad72ced48c
Jayla Jordan <8740826@philasd.org>
2023-03-15 17:13:29 -0400

    roadrunner progress, fixed linear slide difference shaking

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -48,7 +48,7 @@ public class DriveConstants {
      */
     public static double WHEEL_RADIUS = 1.8898; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (motor) speed
-    public static double TRACK_WIDTH = 13; // in
+    public static double TRACK_WIDTH = 12.9; // in
 
     /*
      * These are the feedforward parameters used to model the drive motor behavior. If you are using
@@ -90,8 +90,8 @@ public class DriveConstants {
      */
     public static double MAX_VEL = 40;
     public static double MAX_ACCEL = 40;
-    public static double MAX_ANG_VEL = Math.toRadians(150);
-    public static double MAX_ANG_ACCEL = Math.toRadians(150);
+    public static double MAX_ANG_VEL = 5.200001174797067;
+    public static double MAX_ANG_ACCEL = 4.037147621792714;
 
 
     public static double encoderTicksToInches(double ticks) {
